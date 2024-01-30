51493dfc1877f9984584a200cc25f4e9cb6d04b7
SD191100 <shivamdurgude1@gmail.com>
2024-01-31 02:00:11 +0530

    added user routes for display

--- backend/index.js
+++ backend/index.js
@@ -324,6 +324,39 @@ app.delete('/admin/newsletters/:letterId', auth, async (req, res) => {
   }
 });
 
+app.get('/user/events', async (req, res) => {
+  // gets/shows all events
+  const events = await Event.find({});
+  res.json({ message: 'events Fetched Sucessfully, ', events: events });
+});
+
+app.get('/user/newsletters', async (req, res) => {
+  // gets/shows all news
+  const newsLetters = await Letter.find({});
+  res.json({ message: 'news Fetched Sucessfully, ', newsLetters: newsLetters});
+});
+
+app.get('/user/gallery', async (req, res) => {
+  const photos = await Gallery.find({});
+  res.json({ message: 'images Fetched Sucessfully', photos: photos});
+});
+
+app.get('/user/gallery/:photoId', async (req, res) => {
+  const id = req.params.photoId;
+  const photo = await Gallery.findById(id);
+  if (photo) {
+    res.json({ photo: photo });
+  } else {
+    res.status(404).json({ message: 'Photo not found' });
+  }
+});
+
+app.get('/user/member', async (req, res) => {
+  // gets/shows all members
+  const members = await Member.find({});
+  res.json({ message: 'members Fetched Sucessfully, ', members: members });
+});
+
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 })
