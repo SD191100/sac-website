ba8eac42a66dd6947fb1100f62a4fa058554e084
SD191100 <shivamdurgude1@gmail.com>
2024-02-01 00:46:14 +0530

    added most of the routes for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,12 @@ function App() {
   return (
     <>
       <Router>
-        <Routes >
+        <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/newsletter" element={<Newsletter />} />
+            <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </>
